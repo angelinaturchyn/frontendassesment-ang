146d1ea1ce014aaaa42462fd0519225a098fb1b5
angelinaturchyn <angelinaturchyn@gmail.com>
2023-01-12 01:19:09 -0600

    updated styling

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import axios from "axios";
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
+const theme = createTheme();
 
 function App() {
     const [name, setName] = useState('');
@@ -63,43 +69,49 @@ function App() {
   return (
 
       <div className="App">
-        {errorText && <p style={{ color: "red" }}>Please fill out all required fields </p>}
+      <ThemeProvider theme={theme}>
+              <Container component="main" maxWidth="xs">
+                  <CssBaseline />
+                  <Box
+                      component="form"
+                      sx={{
+                          marginTop: 5,
+                          display: 'flex',
+                          flexDirection: 'column',
+                          alignItems: 'center',
+                          width: 500,
+                          maxWidth: '100%',
+                          '& .MuiTextField-root': { m: 1 },
+                      }}
+                  >
+                      <TextField onChange={(e) => setName(e.target.value)}
+                                 type={"text"}fullWidth label="Full Name" id="fullWidth" />
+                      <TextField onChange={(e) => setEmail(e.target.value)}
+                                 type={"email"}fullWidth label="Email" id="fullWidth" />
+                      <TextField onChange={(e) => setPassword(e.target.value)}
+                                type={"password"}fullWidth label="Password" id="fullWidth" />
 
-        <label>Name:</label>
-        <input
-            minlength="1"
-            onError={() => {
-              "Please enter a name";
-            }}
-            onChange={(e) => setName(e.target.value)}
-            type={"text"}></input>
+          <TextField select onChange={(e) => setOccupation(e.target.value)} fullWidth label="Occupations" id="fullWidth"/>
+              {occupationData &&
+                  occupationData.map((data, index) => {
+                      return <option key={index}>{data}</option>;
+                  })}
 
-        <label>Email</label>
-        <input onChange = {(e) => setEmail(e.target.value)} type={"email"}></input>
 
-        <label>Password</label>
-        <input
-            onChange={(e) => setPassword(e.target.value)}
-            type = {"password"}
-        ></input>
-        <label>Occupations</label>
-        <select onChange={(e) => setOccupation(e.target.value)}>
-          {occupationData &&
-              occupationData.map((data, index) => {
-                return <option key={index}>{data}</option>;
-              })}
-        </select>
-        <label>States</label>
-        <select onChange = {(e) => setState(e.target.value)}>
-          {states &&
-              states.map((data, index) => {
-                return <option key={index}>{data.name}</option>;
-              })}
-        </select>
-        <Button onClick ={() => handleSubmit()} variant="contained" >Submit</Button>
-
-          {submitSuccess && <p style={{ color: "green" }}>Congratulations! Form submitted successfully! </p>}
-      </div>
+          <label>States</label>
+          <select onChange={(e) => setState(e.target.value)}>
+              {states &&
+                  states.map((data, index) => {
+                      return <option key={index}>{data.name}</option>;
+                  })}
+                 </select>
+                   <Button onClick={() => handleSubmit()} fullWidth variant="contained">Submit</Button>
+                      {errorText && <p style={{color: "red"}}>Please fill out all required fields </p>}
+                        {submitSuccess && <p style={{color: "green"}}>Congratulations! Form submitted successfully! </p>}
+                  </Box>
+      </Container>
+</ThemeProvider>
+</div>
   );
 
 }
