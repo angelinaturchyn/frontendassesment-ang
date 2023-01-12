10d8c0b2f56456fbace060f57e2557f8095fa816
angelinaturchyn <angelinaturchyn@gmail.com>
2023-01-11 22:48:05 -0600

    modified payload

--- src/App.js
+++ src/App.js
@@ -10,80 +10,53 @@ import Button from '@mui/material/Button';
 
 
 function App() {
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState();
-  const [states, setStates] = useState();
-  const [state, setState] = useState();
-  const [errorText, setErrorText] = useState();
-  const [occupationData, setOccupationData] = useState();
-  const [occupation, setOccupation] = useState();
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState();
+    const [states, setStates] = useState();
+    const [state, setState] = useState();
+    const [errorText, setErrorText] = useState();
+    const [occupationData, setOccupationData] = useState();
+    const [occupation, setOccupation] = useState();
 
 
-  const handleSubmit = async () => {
-          try {
-              await axios.post("https://frontend-take-home.fetchrewards.com/form", {
-                      name: name,
-                      email: email,
-                      password: password,
-                      occupation: occupation,
-                      states: state,
-              });
+    const handleSubmit = async () => {
 
-          } catch (error) {
-              console.log(error.response);
-
-              // Check if it's HTTP 400  error
-              if (error.response.status === 400) {
-                  console.log(`HTTP 400 error occured`);
-              }
-              // You can get response data (mostly the reason would be in it)
-              if (error.response.data) {
-                  console.log(error.response.data);
-              }
-              // TODO: throw error if you want to handle it somewhere
-              // throw error;
-          }
-      };
+        axios.post("https://frontend-take-home.fetchrewards.com/form", {
+                "name": name,
+                "email": email,
+                "password": password,
+                "occupation": occupation,
+                "state": state,
+            },
+        )
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                window.location = "/retrieve" //This line of code will redirect you once the submission is succeed
+            })
 
 
-      // axios.post("https://frontend-take-home.fetchrewards.com/form", JSON.stringify({
-      //     name: name,
-      //     email: email,
-      //     password: password,
-      //     occupation: occupation,
-      //     states: state,
-      // },
-      //        ),
-      //    )
-      //    .then(res=>{
-      //        console.log(res);
-      //        console.log(res.data);
-      //        window.location = "/retrieve" //This line of code will redirect you once the submission is succeed
-      //    })
+        if (!name || !state || !occupation || !password) {
+            setErrorText("missing data");
+            return;
+        }
 
+    }
+    useEffect(() => {
 
-  // if (!name || !state || !occupation || !password) {
-    //   setErrorText("missing data");
-    //   return;
-    //  }
-
-
-
-  useEffect(() => {
-
-       axios.get("https://frontend-take-home.fetchrewards.com/form")
-        .then(function (response) {
-          // handle success
-          console.log(response);
-          setOccupationData(response.data.occupations);
-          setStates(response.data.states);
-        })
-        .catch(function (error) {
-          // handle error
-          console.log( "Please enter a name");
-        });
-  }, []);
+        axios.get("https://frontend-take-home.fetchrewards.com/form")
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                setOccupationData(response.data.occupations);
+                setStates(response.data.states);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log("Please enter a name");
+            });
+    }, []);
 
 
 
